0870f387b85f8398c2ad6901571e455100b0566c
Yusuke Komatsu <komatsu-yusuke@dmm.com>
2016-03-23 16:30:08 +0900

    golang files are gofmted

--- base65536_test.go
+++ base65536_test.go
@@ -1,55 +1,55 @@
 package base65536
 
-import(
-    // test library
-    "testing"
-    // other libraries
-    "strings"
+import (
+	// test library
+	"testing"
+	// other libraries
+	"strings"
 )
 
 func TestEncode(t *testing.T) {
-    s := "hello world"
-    expected := "驨ꍬ啯𒁷ꍲᕤ"
+	s := "hello world"
+	expected := "驨ꍬ啯𒁷ꍲᕤ"
 
-    actual := Encode(s)
-    if (actual != expected) {
-        t.Errorf("This encoder is not correct.\nexpected: %s\nactual: %s", expected, actual)
-    }
+	actual := Encode(s)
+	if actual != expected {
+		t.Errorf("This encoder is not correct.\nexpected: %s\nactual: %s", expected, actual)
+	}
 }
 
 func TestDecode(t *testing.T) {
-    s := "驨ꍬ啯𒁷ꍲᕤ"
-    expected := "hello world"
+	s := "驨ꍬ啯𒁷ꍲᕤ"
+	expected := "hello world"
 
-    actual := Decode(s)
-    if (actual != expected) {
-        t.Errorf("This encoder is not correct.\nexpected: %s\nactual: %s", expected, actual)
-    }
+	actual := Decode(s)
+	if actual != expected {
+		t.Errorf("This encoder is not correct.\nexpected: %s\nactual: %s", expected, actual)
+	}
 }
 
 func TestCorrectnessCheckingJapaneseAndEnglish(t *testing.T) {
-    str := "Why Japanese!? おかしいやろぉ！！ 躊躇って書いてる間に躊躇してまぅわ！"
-    encode_str := Encode(str)
-    decode_str := Decode(encode_str)
+	str := "Why Japanese!? おかしいやろぉ！！ 躊躇って書いてる間に躊躇してまぅわ！"
+	encode_str := Encode(str)
+	decode_str := Decode(encode_str)
 
-    if (str != decode_str) {
-        t.Errorf("This encode/decode is not correct.\nbefore: %s\nafter: %s", str, decode_str)
-    }
+	if str != decode_str {
+		t.Errorf("This encode/decode is not correct.\nbefore: %s\nafter: %s", str, decode_str)
+	}
 }
 
 func TestCorrectnessCheckingAllString(t *testing.T) {
-    str_map := []string{
-        "1234567890",
-        "abcdefghijklmnopqrstuvwxyz",
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゐゆゑよらりるれろわをん",
-        "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤヰユヱヨラリルレロワヲン",
-    }
-    str := strings.Join(str_map, "")
-    encode_str := Encode(str)
-    decode_str := Decode(encode_str)
+	str_map := []string{
+		"1234567890",
+		"abcdefghijklmnopqrstuvwxyz",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゐゆゑよらりるれろわをん",
+		"アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤヰユヱヨラリルレロワヲン",
+	}
+	str := strings.Join(str_map, "")
+	encode_str := Encode(str)
+	decode_str := Decode(encode_str)
 
-    if (str != decode_str) {
-        t.Errorf("This encode/decode is not correct.\nbefore: %s\nafter: %s", str, decode_str)
-    }
+	if str != decode_str {
+		t.Errorf("This encode/decode is not correct.\nbefore: %s\nafter: %s", str, decode_str)
+	}
 }
